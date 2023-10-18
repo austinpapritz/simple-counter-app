984e1ce006c963ea56e0fcd57c04e499c52fd97e
Austin Papritz <austin@papritz.dev>
2023-10-18 14:05:48 -0700

    created simple Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <h1>Simple Counter</h1>
+      <p>{{ count }}</p>
+      <button @click="increment">Increment</button>
+      <button @click="decrement">Decrement</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        count: 0,
+      };
+    },
+    methods: {
+      increment() {
+        this.count++;
+      },
+      decrement() {
+        this.count--;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any styles you want here. */
+  </style>
+  
